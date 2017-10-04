c3c572a2faefdf0c9e6341a5d3239c4ab58a791e
Akiyo <flying.unicorn.sf@gmail.com>
2017-10-04 13:11:44 -0700

    initMap is calling weather API. linked dailyMsg1 js

--- assets/script/JavaScript.js
+++ assets/script/JavaScript.js
@@ -1,14 +1,16 @@
 
-  // init location API
-function initMap() {
+$( document ).ready(function(){
+initMap(weatherLocation);
 
+  // init location API
+// function initMap() {
+  function initMap(callback) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-        };
-    console.log(pos)    
+        };  
     var latitude = pos.lat; 
     var longitude = pos.lng;
     //var location = ("{lat: "+latitude+", lng: "+longitude+"}");
@@ -16,13 +18,63 @@ function initMap() {
     localStorage.setItem("longitude", longitude);
     var location = JSON.stringify(pos);
     localStorage.setItem("pos", location);
+    callback(latitude, longitude)
     }); // closing getCurrentPosition
   }; //closing if navigator.geolocation
+}; //initMap
 
-}; //initLocation
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAS0vmS8AcOvr9zEvifAg2y8xOkbMmOjAQ",
+    authDomain: "bootcamp-group-project-1.firebaseapp.com",
+    databaseURL: "https://bootcamp-group-project-1.firebaseio.com",
+    projectId: "bootcamp-group-project-1",
+    storageBucket: "bootcamp-group-project-1.appspot.com",
+    messagingSenderId: "828305776803"
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+function initApp() {
+      console.log("auth")
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+          document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+          // [END_EXCLUDE]
+        } else {
+          // User is signed out.
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+          document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+          document.getElementById('quickstart-account-details').textContent = 'null';
+          document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authstatelistener]
+//      document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+    }
 
 
-$( document ).ready(function(){
+//end Auth
+  //$(document).on('click', ".user-login", initApp());
 
   //start with all the pages closed 
   $('.menu').hide();
@@ -53,7 +105,6 @@ $( document ).ready(function(){
   });
   //open weather page
   $('#menu').on("click", '.green', function () {
-    console.log("i'm in green")
     $('#welcome').hide();
     $('#weather').show(); loadMap();
     $('#profile').hide();
@@ -62,7 +113,6 @@ $( document ).ready(function(){
 
   //open pokemon page
   $('#menu').on("click", '.red', function () {
-    console.log("i'm in red")
     $('#welcome').hide();
     $('#weather').hide();
     $('#profile').hide();
@@ -77,7 +127,6 @@ $('#menu').on("click", '.lightblue', function () {
   $('#pokemon').hide();
 });
 
-$(document).on('laod', ".pokeball-container", weatherLocation());
 
 //map and weather page 
 //Jimmy's section modified 
@@ -96,8 +145,6 @@ function loadMap() {
   infoWindow = new google.maps.InfoWindow;
 
   if (navigator.geolocation) {
-
-      // infoWindow.setPosition(pos);
 
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
@@ -120,36 +167,35 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }; // closing handleLocationError
 
-function weatherLocation() {
+
+function weatherLocation(lat, long) {
   console.log("I'm in weatherLocation")
-var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var latty = localStorage.getItem("latitude");
-var longy = localStorage.getItem("longitude");
-// Here we are building the URL we need to query the database
+  var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  // var latty = localStorage.getItem("latitude");
+  // var longy = localStorage.getItem("longitude");
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"lat="+latty+"&lon="+longy+"&appid=" + APIKey;
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-// We store all of the retrieved data inside of an object called "response"
-.done(function(response) {
-  // Log the queryURL
-  console.log(queryURL);
-  // Log the resulting object
-  console.log(response);
-  var windSpeed = response.wind.speed;
-  var humidity = response.main.humidity;
-  var temperature = (response.main.temp * 9 / 5 - 459.67).toFixed(2);
-  var city = response.name;
-  localStorage.setItem("windSpeed", windSpeed);
-  localStorage.setItem("humidity", humidity);
-  localStorage.setItem("temperature", temperature);
-  localStorage.setItem("city", city);
+  var latty = lat;
+  var longy = long;
 
-});
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latty+"&lon="+longy+"&appid="+APIKey;
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  // We store all of the retrieved data inside of an object called "response"
+  .done(function(response) {
+    // Log the resulting object
+    var windSpeed = response.wind.speed;
+    var humidity = response.main.humidity;
+    var temperature = (response.main.temp * 9 / 5 - 459.67).toFixed(2);
+    var city = response.name;
+    localStorage.setItem("windSpeed", windSpeed);
+    localStorage.setItem("humidity", humidity);
+    localStorage.setItem("temperature", temperature);
+    localStorage.setItem("city", city);
+  });
 } //closing weather location 
 
 // load text to the weather div
--- assets/script/dailyMsg1.js
+++ assets/script/dailyMsg1.js
@@ -1,29 +1,14 @@
 $(document).ready(function(){
 	// pokemon images
 	var pokeImg = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png","11.png", "12.png", "13.png", "14.png", "15.png", "16.png","17.png", "18.png", "19.png", "20.png","21.png", "22.png", "23.png", "24.png", "25.png", "26.png", "27.png", "28.png", "29.png", "30.png", "31.png", "32.png", "33.png", "34.png", "35.png", "36.png", "37.png", "38.png", "39.png", "40.png", "41.png", "42.png", "43.png", "44.png", "45.png", "46.png", "47.png", "48.png", "49.png", "50.png", "51.jpg", "52.png", "53.png", "54.jpg", "55.png", "56.png", "57.png", "58.png", "59.png", "60.png", "61.png", "62.png", "63.png", "64.png", "65.png", "66.png","67.png", "68.png", "69.png", "70.png"]
-	var pokeImgPath = "../images/PokeIcon/"
+	var pokeImgPath = "assets/images/PokeIcon/"
 
 	var randomImg = pokeImg[Math.floor(Math.random() * pokeImg.length)]
 	var randomImgPath = pokeImgPath + randomImg
 
 	$(".pokeImg").append("<img src=" + randomImgPath + " width='800px'>")
 	
-	// weather message
-	var APIKey = "166a433c57516f51dfab1f7edaed8413";
-    
-   	var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat={“+latitude+“}&lon={“+longitude+“}&appid=" + APIKey;
-
-   	var queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}"
-
-   	$.ajax({
-   		url: queryURL,
-   		method: "GET"
-   	})
-   	.done(function(event){
-   		console.log(event)
-   	});
-
-   	//var temperature = event.weather // check console.log(event)
+	var temperature = localStorage.getItem("temperature")
 
    	function showMessage (){
    		dailyMsg = "";

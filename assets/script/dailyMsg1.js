c2580118099ab6138b19f25e5204648869eadf44
Akiyo <flying.unicorn.sf@gmail.com>
2017-10-04 20:25:07 -0700

    got rid of old submit. new font

--- assets/script/dailyMsg1.js
+++ assets/script/dailyMsg1.js
@@ -1,43 +1,44 @@
 $(document).ready(function(){
 	// pokemon images
-	var pokeImg = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png","11.png", "12.png", "13.png", "14.png", "15.png", "16.png","17.png", "18.png", "19.png", "20.png","21.png", "22.png", "23.png", "24.png", "25.png", "26.png", "27.png", "28.png", "29.png", "30.png", "31.png", "32.png", "33.png", "34.png", "35.png", "36.png", "37.png", "38.png", "39.png", "40.png", "41.png", "42.png", "43.png", "44.png", "45.png", "46.png", "47.png", "48.png", "49.png", "50.png", "51.jpg", "52.png", "53.png", "54.jpg", "55.png", "56.png", "57.png", "58.png", "59.png", "60.png", "61.png", "62.png", "63.png", "64.png", "65.png", "66.png","67.png", "68.png", "69.png", "70.png"]
+	var pokeImg = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png","11.png", "12.png", "13.png", "14.png", "15.png", "16.png","17.png", "18.png", "19.png", "20.png","21.png", "22.png", "23.png", "24.png", "25.png", "26.png", "27.png", "28.png", "29.png", "30.png", "31.png", "32.png", "33.png", "34.png", "35.png", "36.png", "37.png", "38.png", "39.png", "40.png", "41.png", "42.png", "43.png", "44.png", "45.png", "46.png", "47.png", "48.png", "49.png", "50.png", "52.png", "53.png", "54.jpg", "55.png", "56.png", "57.png", "58.png", "59.png", "60.png", "61.png", "62.png", "63.png", "64.png", "65.png", "66.png","67.png", "68.png", "69.png", "70.png"]
 	var pokeImgPath = "assets/images/PokeIcon/"
 
 	var randomImg = pokeImg[Math.floor(Math.random() * pokeImg.length)]
 	var randomImgPath = pokeImgPath + randomImg
-   var dailyMsg = "";
-   var temperature = localStorage.getItem("temperature");
-   var windSpeed = localStorage.getItem("windSpeed");
-   var humidity = localStorage.getItem("humidity")
-   $('.user-login').on('click', function(){ 
+  var dailyMsg = "";
+  var temperature = localStorage.getItem("temperature");
+  var windSpeed = localStorage.getItem("windSpeed");
+  var humidity = localStorage.getItem("humidity")
+
+  window.welcome = welcome;
+    function welcome(){ 
 	  $(".pokeImg").append("<img src=" + randomImgPath + " width='400px'>")
      degrees(temperature);
      otherWeather(windSpeed, humidity);
      $(".weatherMsg").text(dailyMsg);
-   });
+  };
    
-
    function degrees(tempF) {
 
-      if (tempF < 36){  
-         dailyMsg = '"Brrrr...Snow Attack! Wear some gloves."'
-         $("body").css("background-image", "url('assets/images/PokeBackground/freezeDay.jpg')")
-      }
-            
-      else if (tempF < 51){ 
-         dailyMsg = '"Pretty chilly over here. Bring a jacket!"'
-          $("body").css("background-image", "url('assets/images/PokeBackground/chillDay.jpg')")
-      }     
-            
-      else if (tempF < 80){ 
-         dailyMsg = '"Beautiful Day! Go out and catc`em all!"'
-         $("body").css("background-image", "url('assets/images/PokeBackground/warmDay.jpg')")
-      }     
+    if (tempF < 36){  
+       dailyMsg = '"Brrrr...Snow Attack! Wear some gloves."'
+       $("body").css("background-image", "url('assets/images/PokeBackground/freezeDay.jpg')")
+    }
+          
+    else if (tempF < 51){ 
+       dailyMsg = '"Pretty chilly over here. Bring a jacket!"'
+        $("body").css("background-image", "url('assets/images/PokeBackground/chillDay.jpg')")
+    }     
+          
+    else if (tempF < 80){ 
+       dailyMsg = '"Beautiful Day! Go out and catc`em all!"'
+       $("body").css("background-image", "url('assets/images/PokeBackground/warmDay.jpg')")
+    }     
 
-      else if (tempF >= 80){ 
-         dailyMsg = '"Find a beach and cool off!"'
-         $("body").css("background-image", "url('assets/images/PokeBackground/hotDay.jpg')")
-      }    
+    else if (tempF >= 80){ 
+       dailyMsg = '"Find a beach and cool off!"'
+       $("body").css("background-image", "url('assets/images/PokeBackground/hotDay.jpg')")
+    }    
    };
 
    function otherWeather(windSpeed, humidity){
--- assets/script/firebaseLoginWithGoogle.js
+++ assets/script/firebaseLoginWithGoogle.js
@@ -17,51 +17,51 @@ $( document ).ready(function(){
     initApp();
   });
 
-function toggleSignIn() {
-      if (!firebase.auth().currentUser) {
-        // [START createprovider]
-        var provider = new firebase.auth.GoogleAuthProvider();
-        // [END createprovider]
-        // [START addscopes]
-        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-        // [END addscopes]
-        // [START signin]
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // [START_EXCLUDE]
-          // document.getElementById('quickstart-oauthtoken').textContent = token;
-          // [END_EXCLUDE]
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // [START_EXCLUDE]
-          if (errorCode === 'auth/account-exists-with-different-credential') {
-            alert('You have already signed up with a different auth provider for that email.');
-            // If you are using multiple auth providers on your app you should handle linking
-            // the user's accounts here.
-          } else {
-            console.error(error);
-          }
-          // [END_EXCLUDE]
-        });
-        // [END signin]
-      } else {
-        // [START signout]
-        firebase.auth().signOut();
-        // [END signout]
-      }
-      // [START_EXCLUDE]
-      document.getElementById('quickstart-sign-in').disabled = true;
-      // [END_EXCLUDE]
+  function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+      // [START createprovider]
+      var provider = new firebase.auth.GoogleAuthProvider();
+      // [END createprovider]
+      // [START addscopes]
+      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      // [END addscopes]
+      // [START signin]
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // [START_EXCLUDE]
+        // document.getElementById('quickstart-oauthtoken').textContent = token;
+        // [END_EXCLUDE]
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('You have already signed up with a different auth provider for that email.');
+          // If you are using multiple auth providers on your app you should handle linking
+          // the user's accounts here.
+        } else {
+          console.error(error);
+        }
+        // [END_EXCLUDE]
+      });
+      // [END signin]
+    } else {
+      // [START signout]
+      firebase.auth().signOut();
+      // [END signout]
     }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-in').disabled = true;
+    // [END_EXCLUDE]
+  }
     // [END buttoncallback]
     /**
      * initApp handles setting up UI event listeners and registering Firebase auth listeners:
@@ -86,6 +86,14 @@ function toggleSignIn() {
           // document.getElementById('quickstart-sign-in').textContent = 'Sign out';
           // document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
           // [END_EXCLUDE]
+
+            // successful authentication hides opening and show menu and welcome page
+          
+          $('.opening').hide();
+          $('.menu').show();
+          $('#welcome').show();
+          welcome();
+       
         } else {
           // User is signed out.
           // [START_EXCLUDE]
@@ -97,7 +105,7 @@ function toggleSignIn() {
         }
         // [START_EXCLUDE]
         document.getElementById('quickstart-sign-in').disabled = false;
-        
+
         // [END_EXCLUDE]
       });
       // [END authstatelistener]
